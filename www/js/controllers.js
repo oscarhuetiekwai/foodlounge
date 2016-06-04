4231688db1f40529165cb2c517dc308cffd9aaa3
Oscar Hue <tiekwai@gmail.com>
2016-06-04 12:45:28 +0800

    1st commit

---
+++ www/js/controllers.js
@@ -0,0 +1,62 @@
+angular.module('app.controllers', [])
+  
+.controller('mapCtrl', function($scope) {
+
+})
+   
+.controller('favouriteCtrl', function($scope) {
+
+})
+   
+.controller('orderHistoryCtrl', function($scope) {
+
+})
+         
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('profileSettingCtrl', function($scope) {
+
+})
+   
+.controller('foodStallCtrl', function($scope) {
+
+})
+   
+.controller('orderNowCtrl', function($scope) {
+
+})
+   
+.controller('loungeCtrl', function($scope) {
+
+})
+   
+.controller('checkOutCtrl', function($scope) {
+
+})
+   
+.controller('receiptCtrl', function($scope) {
+
+})
+   
+.controller('orderListCtrl', function($scope) {
+
+})
+   
+.controller('foodStallReviewsCtrl', function($scope) {
+
+})
+   
+.controller('loungeCommentCtrl', function($scope) {
+
+})
+   
+.controller('bookingCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,153 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.map', {
+    url: '/map',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/map.html',
+        controller: 'mapCtrl'
+      }
+    }
+  })
+
+  .state('favourite', {
+    url: '/favourite',
+    templateUrl: 'templates/favourite.html',
+    controller: 'favouriteCtrl'
+  })
+
+  .state('tabsController.orderHistory', {
+    url: '/orderhistory',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/orderHistory.html',
+        controller: 'orderHistoryCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/tab',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/dashboard',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('profileSetting', {
+    url: '/profilesetting',
+    templateUrl: 'templates/profileSetting.html',
+    controller: 'profileSettingCtrl'
+  })
+
+  .state('tabsController.foodStall', {
+    url: '/fooddetails',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/foodStall.html',
+        controller: 'foodStallCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.orderNow', {
+    url: '/ordernow',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/orderNow.html',
+        controller: 'orderNowCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.lounge', {
+    url: '/lounge',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/lounge.html',
+        controller: 'loungeCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.checkOut', {
+    url: '/checkout',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/checkOut.html',
+        controller: 'checkOutCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.receipt', {
+    url: '/receipt',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/receipt.html',
+        controller: 'receiptCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.orderList', {
+    url: '/orderlist',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/orderList.html',
+        controller: 'orderListCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.foodStallReviews', {
+    url: '/foodstallreviews',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/foodStallReviews.html',
+        controller: 'foodStallReviewsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.loungeComment', {
+    url: '/loungecomment',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/loungeComment.html',
+        controller: 'loungeCommentCtrl'
+      }
+    }
+  })
+
+  .state('booking', {
+    url: '/booking',
+    templateUrl: 'templates/booking.html',
+    controller: 'bookingCtrl'
+  })
+
+$urlRouterProvider.otherwise('/dashboard')
+
+  
+
+});

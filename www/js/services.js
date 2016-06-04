9cd262bb8ee849f5d98f15aa5bf576a0bd3928b1
Mark Anthony Gapoy <mark@accordiasolution.com>
2016-06-04 15:17:05 +0800

    Added cordova-plugin-whitelist
    Added whitelist plugin for the google maps to work

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,18 @@
 angular.module('app.services', [])
+.factory('Camera', function($q) {
 
-.factory('BlankFactory', [function(){
+   return {
+      getPicture: function(options) {
+         var q = $q.defer();
 
-}])
+         navigator.camera.getPicture(function(result) {
+            q.resolve(result);
+         }, function(err) {
+            q.reject(err);
+         }, options);
 
-.service('BlankService', [function(){
+         return q.promise;
+      }
+   }
 
-}]);
-
+});
